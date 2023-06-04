4a1bdda7317112b4e2675119876c29cfbe020497
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2023-06-04 13:08:35 -0500

    Pantalla Estudiante seccion Navbar

--- view/view_home.py
+++ view/view_home.py
@@ -2,7 +2,7 @@ from tkinter import *
 import variable.var_global as vg
 
 def vistaEstudiante():
-    global img_salir
+    global img_salir, img_add, img_read, img_update, img_delete
 
     #####
     view_student=Toplevel()
@@ -30,5 +30,21 @@ def vistaEstudiante():
     f_body.place(x=0,y=100)
 
     ############# Navbar
-    f_navbar=Frame(view_student,width=800,height=100,bg="green")
+    f_navbar=Frame(view_student,width=800,height=100,bg="white")
     f_navbar.place(x=0,y=400)
+
+    img_add = PhotoImage(file='./img/img-add.png')
+    btn_add=Button(f_navbar,image=img_add,width=200,pady=7,text="Crear Estudiante",bg="white",fg="black",border=0,font=(vg.g_font,10),compound=BOTTOM)
+    btn_add.place(x=0,y=2)
+
+    img_read = PhotoImage(file='./img/img-read.png')
+    btn_read=Button(f_navbar,image=img_read,width=200,pady=7,text="Leer Estudiante",bg="white",fg="black",border=0,font=(vg.g_font,10),compound=BOTTOM)
+    btn_read.place(x=200,y=2)
+
+    img_update = PhotoImage(file='./img/img-update.png')
+    btn_update=Button(f_navbar,image=img_update,width=200,pady=7,text="Actualizar Estudiante",bg="white",fg="black",border=0,font=(vg.g_font,10),compound=BOTTOM)
+    btn_update.place(x=400,y=2)
+
+    img_delete = PhotoImage(file='./img/img-delete.png')
+    btn_delete=Button(f_navbar,image=img_delete,width=200,pady=7,text="Eliminar Estudiante",bg="white",fg="black",border=0,font=(vg.g_font,10),compound=BOTTOM)
+    btn_delete.place(x=600,y=2)
